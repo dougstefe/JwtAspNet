242a95bf5c43c5403e4f202d5394505879f5472d
Douglas Lessa <dougstefe@gmail.com>
2023-09-01 16:50:38 -0300

    feat: setup jwt authentication and policies

--- Program.cs
+++ Program.cs
@@ -14,7 +14,28 @@ builder.Services.AddTransient<ITokenService, TokenService>();
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+builder.Services.AddAuthentication(x => 
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x => 
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Settings.PrivateKey),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddAuthorization(x => {
+    x.AddPolicy("user", policy => policy.RequireRole("user"));
+    x.AddPolicy("admin", policy => policy.RequireRole("admin"));
+});
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
